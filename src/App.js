1a30f445ae3a27d218ac0ec41aea36a325d2a30a
Ari Simon <arisimon2@gmail.com>
2019-02-02 15:41:16 -0800

    created navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import "./scss/_styles.scss";
+import Navbar from './components/navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="row" />
+        <Navbar />
       </div>
     );
   }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,2 +1,28 @@
-import React from "react";
-import "./scss/_navbar.scss";
+import React, { Component } from "react";
+// import {Link} from 'react-router-dom';
+import "../scss/components/_navbar.scss";
+
+class Navbar extends Component {
+	render() {
+		return (
+			<div className="nav-container">
+				<ul>
+					<li>
+						<a href="#">Home</a>
+					</li>
+					<li>
+						<a href="#">Apps</a>
+					</li>
+					<li>
+						<a href="#">Login</a>
+					</li>
+					<li>
+						<a href="#">Register</a>
+					</li>
+				</ul>
+			</div>
+		);
+	}
+}
+
+export default Navbar;
