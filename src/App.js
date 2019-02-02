88ac0e4af8eafe81938c0025d16657c37556cb13
Ari Simon <arisimon2@gmail.com>
2019-02-02 13:54:07 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./scss/_styles.scss";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>College App Tracker</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
