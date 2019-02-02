2124c07abbb5fc298a8403c6bed2d7ea7b66b75e
Ari Simon <arisimon2@gmail.com>
2019-02-02 14:26:07 -0800

    set up stylesheets and folder structure

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>College App Tracker</h1>
+        <div className="row" />
       </div>
     );
   }
