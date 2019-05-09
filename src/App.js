ed1d45480c18b781afdbbe3c18725597ce283b43
Ari Simon <arisimon2@gmail.com>
2019-05-09 12:34:46 -0700

    added 404 page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./app.css";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import Navbar from "./components/navbar";
 // import Footer from "./components/footer";
--- src/components/404.js
+++ src/components/404.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 function NotFound() {
-	return <h1>404 - Sorry this page is not found</h1>;
+	return <h1>404 - Sorry this page cannot be found</h1>;
 }
 
 export default NotFound;
